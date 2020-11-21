54ef9380f8c345835db1ca7d6632be2703f8ffe3
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-11-21 22:59:42 +0530

    error fixed

--- application/__init__.py
+++ application/__init__.py
@@ -7,10 +7,7 @@ from config import Configs
 app = Flask(__name__)
 app.config.from_object(Configs)
 twitter_bp = make_twitter_blueprint()
-app.register_blueprint(twitter_bp, url_prefix="/login")
-
-
-
+app.register_blueprint(twitter_bp, url_prefix="/twlogin")
 login = LoginManager(app)
 login.login_view = 'login'
 from . import routes
--- config.py
+++ config.py
@@ -8,10 +8,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Configs(object):
     #its a key used as a signature key used to make sure the content sent isnt intercepted
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
-    ENV = os.getenv('FLASK_ENV', default='production')
+    ENV = os.environ.get('FLASK_ENV', default='production')
     DEBUG = ENV == 'development'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.getenv('SECRET_KEY', default='octocat')
-    TWITTER_OAUTH_CLIENT_KEY = os.getenv('TWITTER_OAUTH_CLIENT_KEY', default='TWITTER CONSUMER KEY')
-    TWITTER_OAUTH_CLIENT_SECRET = os.getenv('TWITTER_OAUTH_CLIENT_SECRET', default='ENTER TWITTER CONSUMER SECRET')
+    TWITTER_OAUTH_CLIENT_KEY = os.environ.get('TWITTER_OAUTH_CLIENT_KEY', default='TWITTER CONSUMER KEY')
+    TWITTER_OAUTH_CLIENT_SECRET = os.environ.get('TWITTER_OAUTH_CLIENT_SECRET', default='ENTER TWITTER CONSUMER SECRET')
